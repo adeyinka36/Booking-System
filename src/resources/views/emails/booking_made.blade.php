6ece2fb1ab6d16f277f3d9134e8ed6ffedd34440
Adeyinka  Giwa <adeyinka.giwa@telcom.uk>
2023-06-12 12:07:48 +0100

    correcting email sender name

--- src/resources/views/emails/booking_made.blade.php
+++ src/resources/views/emails/booking_made.blade.php
@@ -1,3 +1,3 @@
 <p>Hi {{ $booking->user->name}}, We have received your booking for <stron>{{ $booking->booking_date }}</stron> at <strong>{{ $booking->booking_time }}</strong>.</p>
 <p>Please Email <strong>{{ config('emails.admin_email') }}</strong> for any changes. </p>
-<p>Thanks, {{ config('app.name') }}</p>
+<p>Thanks, Hayden's Garage</p>
